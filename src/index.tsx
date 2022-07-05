8bef9deacbcbe7af4b7e2cbcea72164e83ad317f
Qtrixnet <qtrixnet@yandex.ru>
2022-07-05 11:00:11 +0600

    feat: add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
+import './index.css';
 import ReactDOM from 'react-dom/client';
-import App from './app/App';
+import App from './app/app';
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App/>
+  <HashRouter>
+    <App/>
+  </HashRouter>
 );
