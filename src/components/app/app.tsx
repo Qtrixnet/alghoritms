c0c1c31aba086e473dabddc24e2c2067b27bcd5d
Qtrixnet <qtrixnet@yandex.ru>
2022-07-07 14:59:36 +0600

    feat: remove css frameworks

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,16 +1,15 @@
 import './app.scss';
 import {FC} from "react";
-import {Container} from "@mui/material";
 import Header from "../header/header";
 import Main from "../main/main";
 
 
 const App: FC = () => {
   return (
-    <Container maxWidth={false}>
+    <div className="app">
       <Header/>
       <Main/>
-    </Container>
+    </div>
   );
 }
 
